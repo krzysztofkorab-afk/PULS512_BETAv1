6365e88cca6e5272de027b14ae2b04ac06b9f366
krzysztofkorab-afk <krzysztof_korab@o2.pl>
2026-09-21 21:04:48 +0200

    Add files via upload

--- app/src/main/java/pl/lab512/puls512/data/PolishTranslator.kt
+++ app/src/main/java/pl/lab512/puls512/data/PolishTranslator.kt
@@ -1,8 +1,8 @@
 package pl.lab512.puls512.data
 
 import com.google.android.gms.tasks.Tasks
+import com.google.mlkit.common.model.DownloadConditions
 import com.google.mlkit.nl.languageid.LanguageIdentification
-import com.google.mlkit.nl.translate.DownloadConditions
 import com.google.mlkit.nl.translate.TranslateLanguage
 import com.google.mlkit.nl.translate.Translation
 import com.google.mlkit.nl.translate.TranslatorOptions
